b0ca4954d99426a2ccc78454ddd6584bb7f1497d
Cletus Kingdom <cletuskingdom@gmail.com>
2023-07-24 16:16:56 +0100

    user can register

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
 import dotenv from "dotenv";
 import express from "express";
+const mongoose = require("mongoose");
 import bodyParser from 'body-parser'; // Optional, but helpful for parsing request bodies
-
 
 // import mongoose from "mongoose";
 dotenv.config({ path: __dirname + "/../config/.env" });
 
 const app = express();
 const port = process.env.PORT || 2000;
+
+const DB_URL =
+	process.env.NODE_ENV === "production"
+		? process.env.PRODUCTION_DB_URL
+		: process.env.LOCAL_DB_URL;
+
+// database connection
+mongoose.connect(DB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error: any) => console.log(error));
+db.once("open", () => console.log("Connected to the database!"));
+
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 const jwt = require('jsonwebtoken');
+const User = require('./../models/users')
 
 const index = (req: Request, res: Response) => {
 	res.status(200).json({
@@ -14,15 +15,48 @@ const index = (req: Request, res: Response) => {
 	});
 };
 
-const login = (req: Request, res: Response) => {
-	const { email, password } = req.body;
+const login = async (req: Request, res: Response) => {
+	const { name, email, phone, password } = req.body;
 
-	res.status(200).json({
-		"response_code": "200",
-		"response_message": "Successful",
-		"data": password
-	});
+	try {
+		const user = await User.create({ name, email, phone, password });
+
+		res.status(201).json({
+			"response_code": "201",
+			"response_message": "Successful",
+			"data": user
+		});
+	} catch (error: any) {
+
+		res.status(404).json({
+			"response_code": "404",
+			"response_message": error.message
+		});
+	}
 };
+
+// const addUsers = (req, res) => {
+// 	const users = new User({
+// 		name: req.body.name,
+// 		email: req.body.email,
+// 		phone: req.body.phone,
+// 		image: req.file.filename,
+// 		password: req.body.password,
+// 	});
+// 	var save_user = users.save();
+// 	if (save_user) {
+// 		req.session.message = {
+// 			type: "success",
+// 			message: "User added successfully",
+// 		};
+// 		res.redirect("/");
+// 	} else {
+// 		res.json({
+// 			type: "danger",
+// 			message: err.message,
+// 		});
+// 	}
+// };
 
 // Export the controller functions
 module.exports = {
